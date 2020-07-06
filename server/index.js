68183c29de34748235a26f041ca057d1cd4b2ee7
eshoiza <engrjayt200@gmail.com>
2020-07-06 17:45:38 +0100

    added a node server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const authRoute = require('./routes/auth');
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(authRoute);
+
+app.listen(PORT, ()=>{
+    console.log(`server start on port ${PORT}`)
+});
